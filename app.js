56fa7c41052bcd6d8cc05f14e0ef4a1cc90a3e61
jakhongirkholikulov <98052598+jahakholikulov002@users.noreply.github.com>
2022-10-11 01:20:48 +0500

    random user practice

---
+++ app.js
@@ -0,0 +1,26 @@
+/** @format */
+
+const app = Vue.createApp({
+	template: '',
+	data() {
+		return {
+			firstName: 'John',
+			lastName: 'Doe',
+			email: 'jonhdoe@gmail.com',
+			gender: 'male',
+			picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+		};
+	},
+	methods: {
+		async getUser() {
+			const res = await fetch('https://randomuser.me/api');
+			const { results } = await res.json();
+			(this.firstName = results[0].name.first),
+				(this.lastName = results[0].name.last),
+				(this.email = results[0].email),
+				(this.gender = results[0].gender),
+				(this.picture = results[0].picture.large);
+		},
+	},
+});
+app.mount('#app');
